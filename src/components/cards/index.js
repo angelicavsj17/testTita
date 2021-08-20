e48c2944bef31c41d68c508761fa66a7ac258a47
avsj <addin@addintechnologies.co>
2021-08-19 23:24:54 -0500

    styles

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../cards/cards.css'
-import cat from '../../assets/cat.png'
+import cat from '../../assets/gato.png'
 import like from '../../assets/like.png'
-
+import user from '../../assets/user.png'
 
 const Cards = () => {
     return (
@@ -10,7 +10,7 @@ const Cards = () => {
             <div className="container--cards">
                 <div className="box1--cards--post">
                     <div className="container--user--data--post">
-                        <img src={cat} className="img-user--post" />
+                        <img src={user} className="img-user--post" />
                         <h1 className="style--name--user--card">angelica sarmiento</h1></div>
                     <img src={cat} className="style--img--post" />
                 </div>
@@ -21,13 +21,13 @@ const Cards = () => {
                         <h1 className="style--tags">tags</h1>
                         <h1 className="style--tags">tags</h1>
                     </div>
-
                     <div className="container--likes">
                         <img src={like} className="style--like" />
                         <h1 className="style--text--like">45</h1>
+                       
                     </div>
+                     <button>comentarios</button>
                 </div>
-
 
             </div>
         </>
