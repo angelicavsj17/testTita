d3af0c9dae093d3e88e5385c151b4b2ab09e6a09
avsj <addin@addintechnologies.co>
2021-08-20 01:32:39 -0500

    changes

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../navBar/navbar.css'
+import imgU from '../../assets/cat.png'
+
 const NavBar = () => {
     return (
         <div className="container--navBar">
 <div className="container--data--user">
-<h1 className="text--data--user">User</h1>
+    <img src={imgU} className="img--user--header" />
+<h1 className="text--data--user">Angelica sarmiento</h1>
 
 </div>
         </div>
